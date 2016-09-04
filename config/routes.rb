629652967a9bee29d9729145f32fe5f206f3d077
Aaron Porter <aaron.w.porter@gmail.com>
2016-09-04 10:49:31 -0700

    add named routes for about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root "guides#guides"
+
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+
   get '/guides' => 'guides#guides'
 
 end
