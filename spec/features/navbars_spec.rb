e2dd632fb87bf324018ffbd75b49b40124298051
Aaron Porter <aaron.w.porter@gmail.com>
2016-09-04 11:19:52 -0700

    add tests for presence and correct links

--- spec/features/navbars_spec.rb
+++ spec/features/navbars_spec.rb
@@ -1,9 +1,35 @@
 require 'rails_helper'
 
 RSpec.feature "Navbars", type: :feature do
-  it "should be present"
-  it "should have a link to the homepage"
-  it "should have a link to the about page"
-  it "should have a link to the guides page"
-  it "should have a link to the contact page"
+
+  it "should be present" do
+    visit(root_path)
+    page.has_css?("nav .navbar")
+  end
+
+  it "should have a link to the homepage" do
+    visit(about_path)
+    click_link("FGD")
+    expect(page).to have_current_path(root_path)
+  end
+
+  before(:each) do
+    visit(root_path)
+  end
+
+  it "should have a link to the about page" do
+    click_link("About")
+    expect(page).to have_current_path(about_path)
+  end
+
+  it "should have a link to the guides page" do
+    click_link("Guides")
+    expect(page).to have_current_path(guides_path)
+  end
+
+  it "should have a link to the contact page" do
+    click_link("Contact")
+    expect(page).to have_current_path(contact_path)
+  end
+
 end
