6363c86d196549976558c922b05d302ca1f44cf1
Aaron Porter <aaron.w.porter@gmail.com>
2016-09-04 10:30:35 -0700

    change state min length to two chars

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -3,7 +3,7 @@ class Guide < ActiveRecord::Base
   validates :guide_name, presence: true, length: { maximum: 50 }
   validates :address, presence: true, length: { maximum: 50 }
   validates :city, presence: true, length: { maximum: 17 }
-  validates :state, presence: true, length: { minimum: 4, maximum: 12 }
+  validates :state, presence: true, length: { minimum: 2, maximum: 12 }
   validates :phone_number, presence: true, length: { minimum: 10, maximum: 16 }
 
   def self.search(search)
