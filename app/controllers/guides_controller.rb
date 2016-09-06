6e3f8de4c802d547d14a32cdce2db7fd5a8b6911
ayezee33 <ayezee33@gmail.com>
2016-09-06 11:45:37 -0700

    Added changes to edit controller

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -8,6 +8,23 @@ class GuidesController < ApplicationController
     end
   end
 
+  def create
+    @guide = Guide.new guide_params
+
+    unless guide_params.nil?
+      @guide.save
+    end
+  end
+
+  def new
+    @guide = Guide.new
+  end
+
   def edit
   end
+
+  def show
+  end
+
+
 end
