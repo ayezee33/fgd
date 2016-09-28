f052f42079e40a2bb5d53b1f0d939b23ca9b217a
ayezee33 <ayezee33@gmail.com>
2016-09-27 17:35:53 -0700

    Add .env to gitignore and found the correct way to update the slug.

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -6,10 +6,18 @@ class Guide < ActiveRecord::Base
   validates :state, presence: true, length: { minimum: 2, maximum: 12 }
   validates :phone_number, presence: true, length: { minimum: 10, maximum: 16 }
 
+  # extend FriendlyId
+  # belongs_to :guide
+  # friendly_id :guide_name, :use => :history
   extend FriendlyId
   belongs_to :guide
-  friendly_id :guide_name, :use => :history
-  # friendly_id :guide_name, use: [:slugged, :history]
+  friendly_id :guide_name, use: [:slugged, :history]
+
+  def should_generate_new_friendly_id?
+    guide_name_changed?
+  end
+
+
 
   def self.search(search)
     where("guide_name LIKE ?", "%#{search}%")
